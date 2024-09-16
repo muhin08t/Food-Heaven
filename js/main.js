0a7957035546dfb31953ce3187c5422764d3f74a
Mohiuddin <muhin08t@gmail.com>
2024-09-16 22:47:29 +0600

    bug fixed

--- js/main.js
+++ js/main.js
@@ -14,6 +14,44 @@ const updateUI = (params) => {
     let container = document.getElementById("result-container");
     container.innerHTML = '';
     document.getElementById('loading-bar').style.visibility = 'visible';
+    let btnPotato = document.getElementById("potato");
+    let btnSoup = document.getElementById("soup");
+    let btnChicken = document.getElementById("chicken");
+    let btnBeef = document.getElementById("beef");
+
+    if(btnPotato.classList.contains('bg-red-500')){
+        btnPotato.classList.remove('bg-red-500');
+        btnPotato.classList.add('bg-green-200');
+    }
+
+    if(btnSoup.classList.contains('bg-red-500')){
+        btnSoup.classList.remove('bg-red-500');
+        btnSoup.classList.add('bg-green-200');
+    }
+
+    if(btnChicken.classList.contains('bg-red-500')){
+        btnChicken.classList.remove('bg-red-500');
+        btnChicken.classList.add('bg-green-200');
+    }
+
+    if(btnBeef.classList.contains('bg-red-500')){
+        btnBeef.classList.remove('bg-red-500');
+        btnBeef.classList.add('bg-green-200');
+    }
+
+    if(params == "potato") {
+        btnPotato.classList.remove('bg-green-200');
+        btnPotato.classList.add('bg-red-500');
+    } else if(params == "soup") {
+        btnSoup.classList.remove('bg-green-200');
+        btnSoup.classList.add('bg-red-500');
+    } else if(params == "chicken") {
+        btnChicken.classList.remove('bg-green-200');
+        btnChicken.classList.add('bg-red-500');
+    } else {
+        btnBeef.classList.remove('bg-green-200');
+        btnBeef.classList.add('bg-red-500');
+    }
     loadFood(params);
 }
 
